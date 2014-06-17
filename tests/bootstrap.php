0e9149e86ef25ddf16f8d85ae6eeda0d7bc729a5
Yosmany Garcia <yosmanyga@gmail.com>
2014-06-17 10:26:18 -0400

    Cleaned tests bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,4 @@
 <?php
 
-use Doctrine\Common\Annotations\AnnotationRegistry;
-
 $loader = require __DIR__.'/../vendor/autoload.php';
 $loader->add('Cubalider\Test\Component\Money', __DIR__);
-
-AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
